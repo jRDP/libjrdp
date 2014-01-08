043ecf2ccf141885885d75fde941356d16bd1afb
bidifx <sascha@coder-area.de>
2014-01-08 17:43:32 +0100

    Update after_success.sh

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "--DEBUG--"
+echo "$TRAVIS_REPO_SLUG"
+echo "$TRAVIS_JDK_VERSION"
+echo "$TRAVIS_PULL_REQUEST"
+echo "$TRAVIS_BRANCH"
+
 if [ "$TRAVIS_REPO_SLUG" == "jRDP/libjrdp" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
  .travis/publish_javadocs.sh
 fi
